43e39404deb08ff85d7296895d8f7615296340e0
Sergey Denisiuk <seruy722@gmail.com>
2024-02-19 11:05:52 +0200

    add table to app

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,9 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-	const request = await fetch('https://dummyjson.com/products/50');
+	const request = await fetch('https://dummyjson.com/products?limit=20');
 	const response = await request.json();
 	console.log('response', response);
 	return {
-		products: response
+		products: response.products
 	};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	// import { TinySlider } from 'svelte-tiny-slider';
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
@@ -13,16 +14,90 @@
 		>https://dummyjson.com/docs/products</a
 	>
 </p>
-<div><img src={data.products.thumbnail} alt="thumbnailFromList" /></div>
-<div>ID {data.products.id}</div>
-<div>TITLE {data.products.title}</div>
-<div>Description {data.products.description}</div>
-<div>Price {data.products.price} $</div>
 
-<div class="h-96 carousel carousel-vertical rounded-box">
-	{#each data.products.images as imageSrc}
-		<div class="carousel-item h-full">
-			<img src={imageSrc} class="rounded-box" alt="" />
-		</div>
+{#each data.products as product}
+	<div><img src={product.thumbnail} alt="thumbnailFromList" /></div>
+	<div>ID {product.id}</div>
+	<div>TITLE {product.title}</div>
+	<div>Description {product.description}</div>
+	<div>Price {product.price} $</div>
+{/each}
+
+<!-- <TinySlider>
+	{#each data.products.images as item}
+		<img src={item} alt="" />
 	{/each}
-</div>
+
+	<div slot="controls" let:setIndex let:currentIndex>
+		{#each data.products.images as _, i}
+			<button
+				class:active={i == currentIndex}
+				on:click={() => setIndex(i)}
+				on:focus={() => setIndex(i)}
+			>
+				<img src={_} alt="" height="60" />
+			</button>
+		{/each}
+	</div>
+</TinySlider> -->
+
+<!-- <div class="overflow-x-auto">
+	<table class="table">
+		<thead>
+			<tr>
+				<th>
+					<label>
+						<input type="checkbox" class="checkbox" />
+					</label>
+				</th>
+				<th>Name</th>
+				<th>Job</th>
+				<th>Favorite Color</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.products.images as imageSrc}
+				<p>{imageSrc}</p>
+				<tr>
+					<th>
+						<label>
+							<input type="checkbox" class="checkbox" />
+						</label>
+					</th>
+					<td>
+						<div class="flex items-center gap-3">
+							<div class="avatar">
+								<div class="mask mask-squircle w-12 h-12">
+									<img src={imageSrc} alt="Avatar Tailwind CSS Component" />
+								</div>
+							</div>
+							<div>
+								<div class="font-bold">Hart Hagerty</div>
+								<div class="text-sm opacity-50">United States</div>
+							</div>
+						</div>
+					</td>
+					<td>
+						Zemlak, Daniel and Leannon
+						<br />
+						<span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+					</td>
+					<td>Purple</td>
+					<th>
+						<button class="btn btn-ghost btn-xs">details</button>
+					</th>
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			<tr>
+				<th></th>
+				<th>Name</th>
+				<th>Job</th>
+				<th>Favorite Color</th>
+				<th></th>
+			</tr>
+		</tfoot>
+	</table>
+</div> -->
