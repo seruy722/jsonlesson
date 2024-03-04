be78315f34bb16917b3e3ac54a757fe5fdbba254
Sergey Denisiuk <seruy722@gmail.com>
2024-03-04 10:53:17 +0200

    add arrow block

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -204,27 +204,92 @@
 	</div>
 	<div class="drawer-side z-10">
 		<label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
-		<ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+		<ul class="menu p-4 w-150 min-h-full bg-base-200 text-base-content">
 			<!-- Sidebar content here -->
-			{#each productsInCart as product}
-				<div class="flex items-center gap-3">
-					<div class="avatar">
-						<div class="mask mask-squircle w-12 h-12">
-							<img src={product.thumbnail} alt="Avatar Tailwind CSS Component" />
+			{#if productsInCart.length}
+				{#each productsInCart as product}
+					<div class="flex items-center gap-3">
+						<div class="avatar">
+							<div class="mask mask-squircle w-12 h-12">
+								<img src={product.thumbnail} alt="Avatar Tailwind CSS Component" />
+							</div>
+						</div>
+						<div>
+							<div class="font-bold">{product.title}</div>
+							<div class="text-sm opacity-50 text-red-500 line-through">
+								{product.price} $
+							</div>
+							<div class="text-sm opacity-50">
+								{product.price - (product.price / 100) * product.discountPercentage} $
+							</div>
+						</div>
+						<div class="join">
+							<button class="btn join-item">
+								<svg
+									class="h-10 w-10"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M11.7071 4.29289C12.0976 4.68342 12.0976 5.31658 11.7071 5.70711L6.41421 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H6.41421L11.7071 18.2929C12.0976 18.6834 12.0976 19.3166 11.7071 19.7071C11.3166 20.0976 10.6834 20.0976 10.2929 19.7071L3.29289 12.7071C3.10536 12.5196 3 12.2652 3 12C3 11.7348 3.10536 11.4804 3.29289 11.2929L10.2929 4.29289C10.6834 3.90237 11.3166 3.90237 11.7071 4.29289Z"
+										fill="#000000"
+									/>
+								</svg>
+							</button>
+							<div>
+								<div>
+									<input type="number" class="input input-bordered join-item w-20" />
+								</div>
+							</div>
+							<button class="btn join-item">
+								<svg
+									class="h-10 w-10"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z"
+										fill="#000000"
+									/>
+								</svg>
+							</button>
 						</div>
 					</div>
-					<div>
-						<div class="font-bold">{product.title}</div>
-						<div class="text-sm opacity-50 text-red-500 line-through">
-							{product.price} $
-						</div>
-						<div class="text-sm opacity-50">
-							{product.price - (product.price / 100) * product.discountPercentage} $
-						</div>
-					</div>
-				</div>
-			{/each}
-			<div>Summ: {productsCartSumm}</div>
+				{/each}
+				<div>Summ: {productsCartSumm}</div>
+			{:else}
+				<svg
+					viewBox="0 0 20 20"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					xmlns:xlink="http://www.w3.org/1999/xlink"
+				>
+					<title>cart_empty [#1163]</title>
+					<desc>Created with Sketch.</desc>
+					<defs> </defs>
+					<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+						<g
+							id="Dribbble-Light-Preview"
+							transform="translate(-300.000000, -2999.000000)"
+							fill="#000000"
+						>
+							<g id="icons" transform="translate(56.000000, 160.000000)">
+								<path
+									d="M264,2839 L260,2839 L258,2839 L258,2851 L246,2851 L246,2853.184 C244.837,2853.597 244,2854.696 244,2856 C244,2857.657 245.343,2859 247,2859 C248.657,2859 250,2857.657 250,2856 C250,2854.696 249.163,2853.597 248,2853.184 L248,2853 L258,2853 L258,2853.184 C256.837,2853.597 256,2854.696 256,2856 C256,2857.657 257.343,2859 259,2859 C260.657,2859 262,2857.657 262,2856 C262,2854.696 261.163,2853.597 260,2853.184 L260,2841 L264,2841 L264,2839 Z M247,2854.951 L247.33,2855.068 C247.664,2855.187 248,2855.521 248,2856 C248,2856.551 247.551,2857 247,2857 C246.449,2857 246,2856.551 246,2856 C246,2855.521 246.336,2855.187 246.67,2855.068 L247,2854.951 L247,2854.951 Z M259,2854.951 C259.547,2855.145 260,2855.339 260,2856 C260,2856.551 259.551,2857 259,2857 C258.449,2857 258,2856.551 258,2856 C258,2855.339 258.453,2855.145 259,2854.951 L259,2854.951 Z"
+									id="cart_empty-[#1163]"
+								>
+								</path>
+							</g>
+						</g>
+					</g>
+				</svg>
+			{/if}
 		</ul>
 	</div>
 </div>
